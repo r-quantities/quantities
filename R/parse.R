2f8e999a12096e12310864edf21481698043d181
Iñaki Úcar <iucar@fedoraproject.org>
2018-08-30 15:14:41 +0200

    update parse docs

--- R/parse.R
+++ R/parse.R
@@ -23,6 +23,9 @@
 #' # quantities are converted to the first unit
 #' parse_quantities(c("12.34(2) m/s", "36.5(1) km/h"))
 #'
+#' # or kept as a list of mixed units
+#' parse_quantities(c("1.02(5) g", "2.51(0.01) V", "(3.23 +/- 0.12) m"))
+#'
 #' @importFrom Rcpp evalCpp
 #' @useDynLib quantities, .registration=TRUE
 #' @export
