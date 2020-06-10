db3546e93d33b44323000124f7826b8537e154e1
Lionel Henry <lionel.hry@gmail.com>
2020-06-10 14:54:36 +0200

    Add coercion methods for quantities

--- R/tidyverse.R
+++ R/tidyverse.R
@@ -5,6 +5,34 @@ vec_restore.quantities <- function(x, to, ...) {
   set_quantities(x, units(to), errors(x), mode = "standard")
 }
 
+vec_ptype2.quantities.quantities <- function(x, y, ...) {
+  x_units <- drop_errors(x)
+  y_units <- drop_errors(y)
+  common <- vctrs::vec_ptype2(x_units, y_units, ...)
+
+  set_quantities(common, units(common), mode = "standard")
+}
+
+vec_cast.quantities.quantities <- function(x, to, ...) {
+  to_units <- units(to)
+
+  # First set units and errors. Must happen first in case this causes
+  # `x` to become fractional (which should cause an error if `to` is
+  # integer).
+  out <- set_units(x, to_units, mode = "standard")
+  out_errors <- errors(out)
+
+  # Now cast base type
+  out_bare <- drop_quantities(out)
+  to_bare <- drop_quantities(to)
+  out <- vctrs::vec_cast(out_bare, to_bare, ...)
+
+  # Set quantities back
+  set_quantities(out, to_units, out_errors, mode = "standard")
+}
+
 register_tidyverse_methods = function() {
   s3_register("vctrs::vec_restore", "quantities")
+  s3_register("vctrs::vec_ptype2", "quantities.quantities")
+  s3_register("vctrs::vec_cast", "quantities.quantities")
 }
--- tests/testthat/test-tidyverse.R
+++ tests/testthat/test-tidyverse.R
@@ -27,3 +27,46 @@ test_that("can slice quantities", {
   )
   expect_equal(vctrs::vec_chop(df), exp)
 })
+
+test_that("quantities have coercion methods", {
+  x <- set_quantities(1:3, "cm", 3:1, mode = "standard")
+  y <- set_quantities(4, "m", 2, mode = "standard")
+  z <- set_quantities(10, "celsius", 20, mode = "standard")
+
+  expect_error(
+    vctrs::vec_ptype_common(x, y, z),
+    class = "vctrs_error_incompatible_type"
+  )
+  expect_error(
+    vctrs::vec_cast_common(z, x, y),
+    class = "vctrs_error_incompatible_type"
+  )
+
+  out <- vctrs::vec_ptype_common(x, y)
+  expect_equal(out, set_quantities(double(), "cm", mode = "standard"))
+  expect_equal(typeof(out), "double")
+
+  out <- vctrs::vec_ptype_common(y, x)
+  expect_equal(out, set_quantities(double(), "m", mode = "standard"))
+  expect_equal(typeof(out), "double")
+
+  out <- vctrs::vec_cast_common(y, x)
+  exp <- list(
+    set_quantities(4, "m", 2, mode = "standard"),
+    set_quantities(c(0.01, 0.02, 0.03), "m", c(0.03, 0.02, 0.01), mode = "standard")
+  )
+  expect_equal(out, exp)
+  expect_equal(sapply(out, typeof), c("double", "double"))
+
+  out <- vctrs::vec_cast_common(x, y)
+  exp <- list(
+    set_quantities(as.double(1:3), "cm", as.double(3:1), mode = "standard"),
+    set_quantities(400, "cm", 200, mode = "standard")
+  )
+  expect_equal(sapply(out, typeof), c("double", "double"))
+
+  expect_error(
+    vctrs::vec_cast(x, set_quantities(0L, "m")),
+    class = "vctrs_error_cast_lossy"
+  )
+})
