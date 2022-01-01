10e72b53368b69e73200bd567bf67390ebb230d9
Iñaki Ucar <iucar@fedoraproject.org>
2022-01-01 22:20:36 +0100

    add compatibility between geom_errors and scale_[x|y]_units (#13)
    * add compatibility between geom_errors and scale_[x|y]_units
    
    * add tests
    
    * bump version, update NEWS

--- R/init.R
+++ R/init.R
@@ -1,4 +1,22 @@
 
 .onLoad = function(libname, pkgname) {
   register_all_s3_methods()
+
+  # geom_errors() compatibility with scale_[x|y]_units()
+  options(errors.compute_statistic = function(data, layout) {
+    scales <- layout$get_scales(data$PANEL[1])
+    if (!is.null(data$xmin_errors)) {
+      units(data$xmin_errors) <- scales$x$units
+      units(data$xmax_errors) <- scales$x$units
+      data$xmin_errors <- as.numeric(data$xmin_errors)
+      data$xmax_errors <- as.numeric(data$xmax_errors)
+    }
+    if (!is.null(data$ymin_errors)) {
+      units(data$ymin_errors) <- scales$y$units
+      units(data$ymax_errors) <- scales$y$units
+      data$ymin_errors <- as.numeric(data$ymin_errors)
+      data$ymax_errors <- as.numeric(data$ymax_errors)
+    }
+    data
+  })
 }
